9698c996c9f5a5446f571e2c68cd5ffc5a17c22f
Hector Vasquez <hectorfabiovasquez@gmail.com>
2021-06-18 13:00:39 -0500

    Finish task 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'countryApp';
+  
+  constructor(public router: Router) {
+    
+  }
+  
+  queryParams() { 
+    //this.router.navigate(['countries'], { queryParams: { name: 'colombia' } }) 
+    this.router.navigate(['countries'], { queryParams: {city: 'medellin', currency: 'COP' } })
+  }
 }
+
+
+
